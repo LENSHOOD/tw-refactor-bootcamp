609fb68c8929e8409a7019015d80bb19951fb087
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-02-21 00:38:41 +0800

    refactor: change qty name to quantity

--- src/main/java/cc/xpbootcamp/warmup/cashier/ItemInfo.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/ItemInfo.java
@@ -3,30 +3,30 @@ package cc.xpbootcamp.warmup.cashier;
 import java.math.BigDecimal;
 
 public class ItemInfo {
-	private String desc;
-	private BigDecimal price;
-	private int qty;
+    private String desc;
+    private BigDecimal price;
+    private int quantity;
 
-	public ItemInfo(String desc, double price, int qty) {
-		super();
-		this.desc = desc;
-		this.price = BigDecimal.valueOf(price);
-		this.qty = qty;
-	}
+    public ItemInfo(String desc, double price, int quantity) {
+        super();
+        this.desc = desc;
+        this.price = BigDecimal.valueOf(price);
+        this.quantity = quantity;
+    }
 
-	public String getDescription() {
-		return desc;
-	}
+    public String getDescription() {
+        return desc;
+    }
 
-	public BigDecimal getPrice() {
-		return price;
-	}
+    public BigDecimal getPrice() {
+        return price;
+    }
 
-	public int getQuantity() {
-		return qty;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
     public BigDecimal totalAmount() {
-		return price.multiply(BigDecimal.valueOf(qty));
+        return price.multiply(BigDecimal.valueOf(quantity));
     }
 }
